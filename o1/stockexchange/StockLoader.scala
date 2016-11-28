a75b60985223454080ff568bfa05be44639c2eca
Aapo Kiiso <aapo@kii.so>
2016-11-28 20:02:43 +0200

    Comments / cleaning up StockLoader

--- o1/stockexchange/StockLoader.scala
+++ o1/stockexchange/StockLoader.scala
@@ -1,20 +1,33 @@
 package o1.stockexchange
 
 import scala.io.Source
-import scala.collection.mutable.Map
+import scala.collection.mutable.{Map => MutableMap}
+import scala.collection.immutable.{Map => ImmutableMap}
 
 class StockLoader(val stocksPath: String) {
   
-  def loadStocksFromFile(stocksFile: String): Map[Int, String] = {
+  /**
+   * Returns map of stock IDs and company names.
+   * 
+   * Map format: stock ID -> Company name
+   * 
+   * Stock ID is nothing else but a numeric ID
+   * used by Kauppalehti to identify stocks.
+   * See "scrape-stocks.js" in this project's root for details.
+   * 
+   * @param stocksFile - path to file to load stock data from,
+   * 										 relative to the stock loader's base path.
+   */
+  def stocksFromFile(stocksFile: String): MutableMap[Int, String] = {
     val filePath = this.stocksPath + StockLoader.DirSeparator + stocksFile
     val stocksSource = Source.fromFile(filePath)
-    val stocks = Map[Int, String]()
+    val stocks = MutableMap[Int, String]()
     
     try {
       for (line <- stocksSource.getLines()) {
         val lineParts = line.split(StockLoader.CsvSeparator)
-        val stockId = lineParts(0).toInt
-        val stockName = lineParts(1)
+        val stockId = this.stockListValue(line, "id").toInt
+        val stockName = this.stockListValue(line, "name")
         stocks(stockId) = stockName
       }
     } finally {
@@ -24,22 +37,33 @@ class StockLoader(val stocksPath: String) {
     stocks
   }
   
-  def loadStock(stockFile: String): Map[String, Double] = {
+  /**
+   * Returns quarterly average prices for a single stock.
+   * We are not interested in day-to-day prices 
+   * (which we have on file), but the average quarterly prices.
+   * 
+   * Map format: Quarter name -> Average price for the quarter
+   * Quarter name is of format "quarter/year", example "3/2006"
+   * 
+   * @param stockFile - path to file to load stock data from,
+   * 										relative to the stock loader's base path.
+   */
+  def stockFromFile(stockFile: String): MutableMap[String, Double] = {
     val filePath = this.stocksPath + StockLoader.DirSeparator + stockFile
     val stockSource = Source.fromFile(filePath)
-    val stockQuarters = Map[String, Double]()
+    val stockQuarters = MutableMap[String, Double]()
     
     try {
-      // Skip first line (header line)
+      // Skip first line (CSV header line)
       val stockDayLines = stockSource.getLines().toVector.tail
       
-      val stockDaysInQuarters = this._groupStockDaysInQuarters(stockDayLines)
+      val stockDaysInQuarters = this.groupStockDaysInQuarters(stockDayLines)
       
       // Add quarters to map with average price.
       for (stockQuarter <- stockDaysInQuarters) {
         val quarterName = stockQuarter._1
         val quarterDayLines = stockQuarter._2
-        val quarterAveragePrice = this._getStockDaysAveragePrice(quarterDayLines)
+        val quarterAveragePrice = this.stockDaysAveragePrice(quarterDayLines)
         
         stockQuarters(quarterName) = quarterAveragePrice
       }
@@ -50,37 +74,45 @@ class StockLoader(val stocksPath: String) {
     stockQuarters
   }
   
-  private def _groupStockDaysInQuarters(stockDayLines: Vector[String]) = {
+  private def groupStockDaysInQuarters(stockDayLines: Vector[String]) = {
     val groupedStockDays = stockDayLines.groupBy((line: String) => {
-      val stockDateStr = line.split(StockLoader.CsvSeparator)(0);
-      
+      val stockDateStr = this.stockValue(line, "date")
       // Use quarter name (ex. "2/2015") to group.
-      this._getQuarterNameByDateStr(stockDateStr)
+      this.quarterNameByDateStr(stockDateStr)
     })
     
     groupedStockDays
   }
-  
-  private def _getStockDaysAveragePrice(stockDayLines: Vector[String]) = {
-    val pricesSum = stockDayLines.foldLeft(0.0)((sum: Double, line: String) => {
-      val closingPriceStr = line.split(StockLoader.CsvSeparator)(4)
-      val closingPrice = closingPriceStr.replace(',', '.').toDouble
-      
-      sum + closingPrice
-    })
-    
-    pricesSum / stockDayLines.size
-  }
 
-  private def _getQuarterNameByDateStr(dateStr: String): String = {
+  private def quarterNameByDateStr(dateStr: String): String = {
     val date = StockLoader.StockDateFormat.parse(dateStr)
+    
+    // Use Java's Calendar to reliably get month / year from date str.
+    // Use singleton Calendar instance for convenience.
     val cal = StockLoader.StockDateCalendar
     cal.setTime(date)
     val month = cal.get(java.util.Calendar.MONTH)
     val year = cal.get(java.util.Calendar.YEAR)
     
-    ((month / 3) + 1) + "/" + year
+    this.monthQuarter(month) + "/" + year
   }
+  
+  private def monthQuarter(month: Int) = (month / 3) + 1
+  
+  private def stockDaysAveragePrice(stockDayLines: Vector[String]) = {
+    val pricesSum = stockDayLines.foldLeft(0.0)((sum: Double, line: String) => {
+      val closingPrice = this.stockValue(line, "close").replace(',', '.').toDouble
+      sum + closingPrice
+    })
+    
+    pricesSum / stockDayLines.size
+  }
+  
+  private def stockListValue(line: String, columnName: String) = this.csvValue(line, columnName, StockLoader.StockListColumnIndices)
+  
+  private def stockValue(line: String, columnName: String) = this.csvValue(line, columnName, StockLoader.StockColumnIndices)
+  
+  private def csvValue(line: String, columnName: String, columnsList: ImmutableMap[String, Int]) = line.split(StockLoader.CsvSeparator)(columnsList(columnName))
   
 }
 
@@ -88,6 +120,12 @@ object StockLoader {
   
   val DirSeparator = "/"
   val CsvSeparator = ";"
+  
+  val StockListColumns = Vector("id", "name")
+  val StockListColumnIndices = (StockListColumns.zip(StockListColumns.indices)).toMap
+  
+  val StockColumns = Vector("date", "open", "high", "low", "close", "volume")
+  val StockColumnIndices = (StockColumns.zip(StockColumns.indices)).toMap
   
   val StockDateFormat = new java.text.SimpleDateFormat("dd.MM.yyyy")
   val StockDateCalendar = java.util.Calendar.getInstance()
